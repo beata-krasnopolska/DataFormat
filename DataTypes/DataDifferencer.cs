a0ce52cef88cc2d5cfcafd5b66498e38d33f4031
beata.krasnopolska@gmail.com <beata.krasnopolska@gmail.com>
2019-10-29 21:36:35 +0100

    Add new class

---
+++ DataTypes/DataDifferencer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace DataTypes
+{
+    class DataDifferencer
+    {
+        public  string Differencer(string dataType)
+        {
+            int tryInt;
+            decimal tryDec;
+            string result;
+            if (Int32.TryParse(dataType, out tryInt))
+            {
+                result = "The data type is integer";
+            }
+            else if (Decimal.TryParse(dataType, out tryDec))
+            {
+                result = "The data type is decimal";
+            }
+            else if (dataType == "a" || dataType == "e" || dataType == "i" || dataType == "o" || dataType == "u" || dataType == "y")
+            {
+                result = "The entered data type is a vovel";
+            }
+            else
+            {
+                result = "There is other data type";
+            }
+            return result;
+        }
+
+    }
+}
--- DataTypes/Program.cs
+++ DataTypes/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DataTypes
 {
@@ -13,27 +9,11 @@ namespace DataTypes
             Console.WriteLine("Program takes a character as input and checks the input (lowercase) is a vowel, an integer number or decimal, or any other symbol.");
 
             Console.WriteLine("Enter the data");
-            var dataType = Console.ReadLine();
+            string dataType = Console.ReadLine();
 
-            int tryInt;
-            decimal tryDec;
-            if (Int32.TryParse(dataType, out tryInt))
-            {
-                Console.WriteLine("The data type is integer");
-            }
-            else if (Decimal.TryParse(dataType, out tryDec))
-            {
-                Console.WriteLine("The data type is decimal");
-            }
-            else if (dataType =="a" || dataType == "e" || dataType == "i" || dataType == "o" || dataType == "u" || dataType == "y")
-            {
-                Console.WriteLine("The entered data type is a vovel");
-            }
-            else
-            {
-                Console.WriteLine("There is other data type");
-            }
-            Console.WriteLine();
+            var newData = new DataDifferencer().Differencer(dataType);
+
+            Console.WriteLine(newData);
             Console.ReadKey();
         }
     }
